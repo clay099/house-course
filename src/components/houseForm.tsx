7a83df4998860190bccc27bab78b71d7ace98fc5
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-01-19 14:29:22 -0800

    initalize apollo & upload image

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
-// import { useMutation, gql } from "@apollo/client";
+import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
 import Link from "next/link";
 // import { Image } from "cloudinary-react";
@@ -13,7 +13,40 @@ import { SearchBox } from "./searchBox";
 //   UpdateHouseMutation,
 //   UpdateHouseMutationVariables,
 // } from "src/generated/UpdateHouseMutation";
-// import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+import { CreateSignatureMutation } from "src/generated/CreateSignatureMutation";
+
+const SIGNATURE_MUTATION = gql`
+  mutation CreateSignatureMutation {
+    createImageSignature {
+      signature
+      timestamp
+    }
+  }
+`;
+
+async function uploadImage(
+  image: File,
+  signature: string,
+  timestamp: number
+): Promise<IUploadImageResponse> {
+  const url = `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`;
+  const formData = new FormData();
+  formData.append("file", image);
+  formData.append("signature", signature);
+  formData.append("timestamp", timestamp.toString());
+  formData.append("api_key", process.env.NEXT_PUBLIC_CLOUDINARY_KEY ?? "");
+
+  const resp = await fetch(url, {
+    method: "post",
+    body: formData,
+  });
+
+  return resp.json();
+}
+
+interface IUploadImageResponse {
+  secure_url: string;
+}
 
 interface IFormData {
   address: string;
@@ -33,6 +66,9 @@ export default function HouseForm({}: IProps) {
   >({ defaultValues: {} });
 
   const address = watch("address");
+  const [createSignature] = useMutation<CreateSignatureMutation>(
+    SIGNATURE_MUTATION
+  );
 
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter your address" });
@@ -41,7 +77,12 @@ export default function HouseForm({}: IProps) {
   }, [register]);
 
   const handleCreate = async (data: IFormData) => {
-    console.log({ data });
+    const { data: signatureData } = await createSignature();
+    if (signatureData) {
+      const { signature, timestamp } = signatureData.createImageSignature;
+      //post to cloudinary
+      const imageData = await uploadImage(data.image[0], signature, timestamp);
+    }
   };
 
   const onSubmit = (data: IFormData) => {
