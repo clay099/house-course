f86da17a6bc80289e42e76a1f003a7a4a0e4afd7
Clayton Whittaker <clayton.whittaker@gmail.com>
2021-01-21 12:44:42 -0800

    fix overflow scroll on home

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home() {
         <div className="flex">
           <div
             className="w-1/2 pb-4"
-            style={{ maxHeight: "calc(100vh - 64px)", overflowX: "scroll" }}
+            style={{ maxHeight: "calc(100vh - 64px)", overflowY: "scroll" }}
           >
             <HouseList
               houses={lastData ? lastData.houses : []}
